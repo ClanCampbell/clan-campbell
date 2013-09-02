388071d562c59d43f173994abbeeb3acf55b861e
Keith W. Campbell <KeithWCampbell@gmail.com>
2013-09-02 10:10:02 -0400

    update symbol list

--- stock.quotes/src/quotes/Main.java
+++ stock.quotes/src/quotes/Main.java
@@ -5,11 +5,10 @@ import quotes.yahoo.HistoryService;
 public final class Main {
 
 	private static final java.lang.String[] symbols = { "EGHT", "AMD", "ALU", "AAPL", "APM.V", "RPT.V", "BNS.TO",
-			"BCE.TO", "BRK-B", "BRCM", "CWL.TO", "CM.TO", "CWA.V", "CUX.TO", "FTS.TO", "GNV.TO", "GRC.TO", "HSE.TO",
-			"IBM", "JML.V", "JDSU", "JDU.TO", "MAS.V", "MSFT", "NA.TO", "NA-PO.TO", "NBD.TO", "NRTLQ.PK", "ORCL",
-			"PCE.TO", "PPL.TO", "PWT.TO", "PG.TO", "POW.TO", "RMK.TO", "SXG.V", "SGF.TO", "SLF.TO", "SU.TO", "SNCR",
-			"TTWO", "T.TO", "TA.TO", "TRP.TO", "WVNTF.PK", "WPK.TO", "^DJI", "^IXIC", "^GSPC", "^GSPTSE", "XIC.TO",
-			"XIU.TO" };
+			"BCE.TO", "BRK-B", "BRCM", "CWL.TO", "CM.TO", "CWA.V", "FTS.TO", "GNV.TO", "HSE.TO", "IDG.TO", "IBM",
+			"JML.V", "JDSU", "JDU.TO", "MAS.V", "MSFT", "NA.TO", "NA-PO.TO", "NBD.TO", "NRTLQ", "ORCL", "PPL.TO",
+			"PWT.TO", "POW.TO", "PG.TO", "RMK.TO", "SXG.V", "SGF.TO", "SGL.TO", "SLF.TO", "SU.TO", "SNCR", "TTWO",
+			"T.TO", "TA.TO", "TRP.TO", "WPK.TO", "WVNTF", "^DJI", "^IXIC", "^GSPC", "^GSPTSE", "XIC.TO", "XIU.TO" };
 
 	public static void main(String[] args) {
 		Date date;
