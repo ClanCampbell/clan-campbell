9006c1a30828c5f42cf3b30d9afddd72effe895e
Keith W. Campbell <KeithWCampbell@gmail.com>
2015-08-29 09:42:16 -0400

    update list of symbols

--- stock.quotes/src/quotes/Main.java
+++ stock.quotes/src/quotes/Main.java
@@ -4,11 +4,60 @@ import quotes.yahoo.HistoryService;
 
 public final class Main {
 
-	private static final java.lang.String[] symbols = { "EGHT", "AMD", "ALU", "AAPL", "APM.V", "RPT.V", "BNS.TO",
-			"BCE.TO", "BRK-B", "BRCM", "CWL.TO", "CM.TO", "FTS.TO", "HSE.TO", "IDG.TO", "IBM",
-			"JML.V", "JDSU", "MAS.V", "MSFT", "NA.TO", "NBD.TO", "NRTLQ", "ORCL", "PPL.TO",
-			"PWT.TO", "POW.TO", "PG.TO", "RMK.TO", "SXG.V", "SGF.TO", "SGL.TO", "SLF.TO", "SU.TO", "SNCR", "TTWO",
-			"T.TO", "TA.TO", "TRP.TO", "WPK.TO", "WVNTF", "WSP.TO", "^DJI", "^IXIC", "^GSPC", "^GSPTSE", "XIC.TO", "XIU.TO" };
+	private static final java.lang.String[] symbols = { /* */
+			//
+			"EGHT", // <br/>
+			"AMD", // <br/>
+			"ALU", // <br/>
+			"AAPL", // <br/>
+			"APM.V", // <br/>
+			"RPT.V", // <br/>
+			"BNS.TO", // <br/>
+			"BCE.TO", // <br/>
+			"BRK-B", // <br/>
+			"BRCM", // <br/>
+			"CWL.TO", // <br/>
+			"CM.TO", // <br/>
+			"COS.TO", // <br/>
+			"DF.TO", // <br/>
+			"FTS.TO", // <br/>
+			"HSE.TO", // <br/>
+			"IDG.TO", // <br/>
+			"IBM", // <br/>
+			"XIC.TO", // <br/>
+			"JML.V", // <br/>
+			"LITE", // <br/>
+			"MAS.V", // <br/>
+			"MSFT", // <br/>
+			"NA.TO", // <br/>
+			"NBD.TO", // <br/>
+			"NRTLQ", // <br/>
+			"ORCL", // <br/>
+			"PPL.TO", // <br/>
+			"PWT.TO", // <br/>
+			"POW.TO", // <br/>
+			"PG.TO", // <br/>
+			"RMK.TO", // <br/>
+			"SXG.V", // <br/>
+			"SGF.TO", // <br/>
+			"SGL.TO", // <br/>
+			"SLF.TO", // <br/>
+			"SU.TO", // <br/>
+			"SNCR", // <br/>
+			"TTWO", // <br/>
+			"T.TO", // <br/>
+			"TA.TO", // <br/>
+			"TRP.TO", // <br/>
+			"VIAV", // <br/>
+			"WPK.TO", // <br/>
+			"WVNTF", // <br/>
+			"WSP.TO", // <br/>
+			"^DJI", // <br/>
+			"^IXIC", // <br/>
+			"^GSPC", // <br/>
+			"^GSPTSE", // <br/>
+			"XIU.TO" // <br/>
+	};
 
 	public static void main(String[] args) {
 		Date date;
@@ -36,8 +85,10 @@ public final class Main {
 			System.out.format("%-8s %10s\n", // <br/>
 					symbol, "-.--");
 		} else {
-			System.out.format("%-8s %,10.2f %,14d\n", // <br/>
-					symbol, Double.valueOf(quote.getPrice()), Long.valueOf(quote.getVolume()));
+			System.out.format(
+					"%-8s %,10.2f %,14d\n", // <br/>
+					symbol, Double.valueOf(quote.getPrice()),
+					Long.valueOf(quote.getVolume()));
 
 		}
 	}
